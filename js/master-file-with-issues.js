5f8898b490a5189744d7e841d9e7ed98099d8cd4
Viking & Robot <vikingandrobot@gmail.com>
2022-01-21 17:15:24 +0100

    Add buggy file.

---
+++ js/master-file-with-issues.js
@@ -0,0 +1,9 @@
+function dangerousCrypto() {
+  return Math.random() > 0.3 ? 'hash-1234' : 'hash-5678';
+}
+
+function printSomething(number) {
+  if (number = 4) {
+    console.log("Number is not 5");
+  }
+}
